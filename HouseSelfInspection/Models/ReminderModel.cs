b7a504a18eb4cf821d1ea97d82ae222d41a1bcfb
dipp-poudel <dipendra.poudel@gmail.com>
2022-01-21 10:35:51 +1030

    Reminder model created

---
+++ HouseSelfInspection/Models/ReminderModel.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseSelfInspection.Models
+{
+    public class ReminderModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int InspectionScheduleId { get; set; }
+
+
+        [Required]
+        public int HouseId { get; set; }
+
+
+        [Required]
+        public int TenantId { get; set; }
+
+        [Required]
+        public DateTime Inspection_date { get; set; }
+
+        public string Inspection_status { get; set; }
+    }
+}
